6f3f4f2d02284d5fc44fb8101c1a8139805e7680
httpwwww <68337609+httpwwww@users.noreply.github.com>
2020-11-06 17:21:04 -0500

    Update RegisterControllerTest.java

--- test/Control/RegisterControllerTest.java
+++ test/Control/RegisterControllerTest.java
@@ -66,59 +66,59 @@ public class RegisterControllerTest {
         */
         String nombre1 = ("Andrea");
         String apellido1 = ("Castañeda");
-        String strDocumento1 = ("1000000001");
+        int Documento1 = 1000000001;
         String strEmail1 = ("acasta@unal.edu.co");
         String contrasena1 = ("123456");
         String reContrasena1 = ("12345J");
-        assertEquals(validar.verificarRegistro(nombre1,apellido1,strDocumento1,strEmail1,contrasena1,reContrasena1),("Las contraseñas no conciden"));
+        assertEquals(validar.verificarRegistro(nombre1,apellido1,Documento1,strEmail1,contrasena1,reContrasena1),("Las contraseñas no conciden"));
        
         String nombre2 = ("Andre");
         String apellido2 = ("Castañeda");
-        String strDocumento2 = ("1000000002");
+        int Documento2 = 1000000002;
         String strEmail2 = ("acasta@unal.edu.co");
         String contrasena2 = ("123");
         String reContrasena2 = ("123");
-        assertEquals(validar.verificarRegistro(nombre2,apellido2,strDocumento2,strEmail2,contrasena2,reContrasena2),("Longitud contrasena incorrecta"));
+        assertEquals(validar.verificarRegistro(nombre2,apellido2,Documento2,strEmail2,contrasena2,reContrasena2),("Longitud contrasena incorrecta"));
        
         String nombre3 = ("Andre");
         String apellido3 = ("Castañeda");
-        String strDocumento3 = ("1000000003");
+        int    Documento3 = 1000000003;
         String strEmail3 = ("acasta-unal.edu.co");
         String contrasena3 = ("123456");
         String reContrasena3 = ("123456");
-        assertEquals(validar.verificarRegistro(nombre3,apellido3,strDocumento3,strEmail3,contrasena3,reContrasena3),("E-mail inválido"));
-       
+        assertEquals(validar.verificarRegistro(nombre3,apellido3,Documento3,strEmail3,contrasena3,reContrasena3),("E-mail inválido"));
+       /*
         String nombre4 = ("Andre");
         String apellido4 = ("Castañeda");
-        String strDocumento4 = ("1.000.00.00.03");
+        int    Documento4 = 10000000.03 ;
         String strEmail4 = ("acasta@unal.edu.co");
         String contrasena4 = ("123456");
         String reContrasena4 = ("123456");
-        assertEquals(validar.verificarRegistro(nombre4,apellido4,strDocumento4,strEmail4,contrasena4,reContrasena4),("Documento inválido"));
-       
+        assertEquals(validar.verificarRegistro(nombre4,apellido4,Documento4,strEmail4,contrasena4,reContrasena4),("Documento inválido"));
+       */
         String nombre5 = ("Andre");
         String apellido5 = ("Cas");
-        String strDocumento5 = ("1000000003");
+        int Documento5 = 1000000003;
         String strEmail5 = ("acasta@unal.edu.co");
         String contrasena5 = ("123456");
         String reContrasena5 = ("123456");
-        assertEquals(validar.verificarRegistro(nombre5,apellido5,strDocumento5,strEmail5,contrasena5,reContrasena5),("Longitud apellido incorrecta"));
+        assertEquals(validar.verificarRegistro(nombre5,apellido5,Documento5,strEmail5,contrasena5,reContrasena5),("Longitud apellido incorrecta"));
        
         String nombre6 = ("San");
         String apellido6 = ("Castañeda");
-        String strDocumento6 = ("1000000003");
+        int Documento6 = 1000000003;
         String strEmail6 = ("acasta@unal.edu.co");
         String contrasena6 = ("123456");
         String reContrasena6 = ("123456");
-        assertEquals(validar.verificarRegistro(nombre6,apellido6,strDocumento5,strEmail6,contrasena6,reContrasena6),("Longitud nombre incorrecta"));
+        assertEquals(validar.verificarRegistro(nombre6,apellido6,Documento5,strEmail6,contrasena6,reContrasena6),("Longitud nombre incorrecta"));
        
         String nombre7 = ("Andres");
         String apellido7 = ("Castañeda");
-        String strDocumento7 = ("1000000000");
+        int Documento7 = 1000000000;
         String strEmail7 = ("acasta@unal.edu.co");
         String contrasena7 = ("123456");
         String reContrasena7 = ("123456");
-        assertEquals(validar.verificarRegistro(nombre7,apellido7,strDocumento7,strEmail7,contrasena7,reContrasena7),("Error: Usuario ya existente"));
+        assertEquals(validar.verificarRegistro(nombre7,apellido7,Documento7,strEmail7,contrasena7,reContrasena7),("Error: Usuario ya existente"));
        
         
     }
