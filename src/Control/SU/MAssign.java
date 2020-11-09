737d9c6c7956c3f3ffdbf1935711ca97f178e676
Sebastián <jupachonc@unal.edu.co>
2020-11-09 18:50:17 -0500

    Asignación Administrador Laboratorio

--- src/Control/SU/MAssign.java
+++ src/Control/SU/MAssign.java
@@ -5,16 +5,36 @@
  */
 package Control.SU;
 
+import DAO.LaboratorioDAO;
+import DAO.UsuarioDAO;
+import Entidad.Laboratorio;
+import Entidad.Usuario;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableCell;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 /**
  * FXML Controller class
@@ -23,19 +43,105 @@ import javafx.stage.Stage;
  */
 public class MAssign implements Initializable {
 
-    @FXML
-    private JFXButton reserveButton;
-    @FXML
-    private JFXButton reserveButton1;
+    private ObservableList<Laboratorio> dataLabs;
+    private ObservableList<Usuario> dataAdminsIn;
+    private ObservableList<Usuario> dataAdminsOut;
+
     @FXML
     private JFXButton backbtn;
+    @FXML
+    private JFXComboBox<Laboratorio> labList;
+    @FXML
+    private JFXTreeTableView<Usuario> InTable;
+    @FXML
+    private TreeTableColumn<Usuario, String> INombres;
+    @FXML
+    private TreeTableColumn<Usuario, String> IApellidos;
+    @FXML
+    private TreeTableColumn<Usuario, Integer> IDocumento;
+    @FXML
+    private JFXTreeTableView<Usuario> OutTable;
+    @FXML
+    private TreeTableColumn<Usuario, String> ONombres;
+    @FXML
+    private TreeTableColumn<Usuario, String> OApellidos;
+    @FXML
+    private TreeTableColumn<Usuario, Integer> ODocumento;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        getLabs();
+    }
+
+    private void getLabs() {
+        dataLabs = FXCollections.observableList(new LaboratorioDAO().getLabs());
+
+        labList.setItems(dataLabs);
+        labList.setConverter(new StringConverter<Laboratorio>() {
+            @Override
+            public String toString(Laboratorio object) {
+                return object.getID() + "-" + object.getNombre();
+            }
+
+            @Override
+            public Laboratorio fromString(String string) {
+                return findID(Integer.parseInt(string.split("-")[0]));
+            }
+        });
+
+    }
+
+    private void getAdminsInLabTable(Laboratorio lab) {
+
+        dataAdminsIn = FXCollections.observableList(new LaboratorioDAO().getAdminsinLab(lab));
+
+        INombres.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("nombre")
+        );
+        IApellidos.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("apellido")
+        );
+        IDocumento.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("documento")
+        );
+
+        TreeItem<Usuario> root = new RecursiveTreeItem<>(dataAdminsIn, RecursiveTreeObject::getChildren);
+
+        InTable.setRoot(root);
+        InTable.setShowRoot(false);
+    }
+
+    private void getAdminsOutLabTable(Laboratorio lab) {
+
+        dataAdminsOut = FXCollections.observableList(new LaboratorioDAO().getAdminsOutLab(lab));
+
+        ONombres.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("nombre")
+        );
+        OApellidos.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("apellido")
+        );
+        ODocumento.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("documento")
+        );
+
+        TreeItem<Usuario> root = new RecursiveTreeItem<>(dataAdminsOut, RecursiveTreeObject::getChildren);
+
+        OutTable.setRoot(root);
+        OutTable.setShowRoot(false);
+    }
+
+    private Laboratorio findID(int id) {
+        for (Laboratorio lab : dataLabs) {
+            if (lab.getID() == id) {
+                return lab;
+            }
+
+        }
+        return null;
     }
 
     @FXML
@@ -52,6 +158,80 @@ public class MAssign implements Initializable {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onSelectItem(ActionEvent event) {
+        Laboratorio lab = labList.getSelectionModel().getSelectedItem();
+        getAdminsInLabTable(lab);
+        getAdminsOutLabTable(lab);
+
+    }
+
+    @FXML
+    private void toLeft(ActionEvent event) {
+        try {
+            Usuario user = OutTable.getSelectionModel().getSelectedItem().getValue();
+            Laboratorio lab = labList.getSelectionModel().getSelectedItem();
+            if (new LaboratorioDAO().addAdmintoLab(user, lab)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Información");
+                alert.setHeaderText("Se añadió el usuario " + user.getNombre() + " "
+                        + user.getApellido() + " al laboratorio " + lab.getNombre());
+                alert.setContentText(null);
+                alert.showAndWait();
+                getAdminsInLabTable(lab);
+                getAdminsOutLabTable(lab);
+            } else {
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Información");
+                alert.setHeaderText("No se pudo añadir el usuario al laboratorio");
+                alert.setContentText(null);
+                alert.showAndWait();
+
+            }
+        } catch (Exception e) {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Información");
+            alert.setHeaderText("Ocurrió un error");
+            alert.setContentText("Debe seleccionar un laboratorio y un usuario");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void toRight(ActionEvent event) {
+        try {
+            Usuario user = InTable.getSelectionModel().getSelectedItem().getValue();
+            Laboratorio lab = labList.getSelectionModel().getSelectedItem();
+            if (new LaboratorioDAO().removeAdminfromLab(user, lab)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Información");
+                alert.setHeaderText("Se eliminó el usuario " + user.getNombre() + " "
+                        + user.getApellido() + " del laboratorio " + lab.getNombre());
+                alert.setContentText(null);
+                alert.showAndWait();
+                getAdminsInLabTable(lab);
+                getAdminsOutLabTable(lab);
+            } else {
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Información");
+                alert.setHeaderText("No se pudo eliminar el usuario del laboratorio");
+                alert.setContentText(null);
+                alert.showAndWait();
+
+            }
+        } catch (Exception e) {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Información");
+            alert.setHeaderText("Ocurrió un error");
+            alert.setContentText("Debe seleccionar un laboratorio y un usuario");
+            alert.showAndWait();
         }
     }
 
--- src/DAO/LaboratorioDAO.java
+++ src/DAO/LaboratorioDAO.java
@@ -6,6 +6,7 @@
 package DAO;
 
 import Entidad.Laboratorio;
+import Entidad.Usuario;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -58,8 +59,8 @@ public class LaboratorioDAO {
             resultSet.beforeFirst();
 
             while (resultSet.next()) {
-                labs.add(new Laboratorio(resultSet.getInt("ID"), resultSet.getString("Nombre"), 
-                resultSet.getString("Telefono"), resultSet.getString("Ubicacion")));
+                labs.add(new Laboratorio(resultSet.getInt("ID"), resultSet.getString("Nombre"),
+                        resultSet.getString("Telefono"), resultSet.getString("Ubicacion")));
             }
 
         } catch (SQLException ex) {
@@ -76,9 +77,139 @@ public class LaboratorioDAO {
         }
         return labs;
     }
-    
-    public boolean disableLab(Laboratorio lab){
-            int resultSet;
+
+    public ArrayList<Usuario> getAdminsinLab(Laboratorio lab) {
+        ArrayList<Usuario> admins = new ArrayList<>();
+        ResultSet resultSet = null;
+
+        try {
+            connection = DBConnection.getConnection();
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM laboratorio_administrador_1, active_admins "
+                    + "WHERE IDAdministrador = Documento "
+                    + "AND IDLaboratorio = " + lab.getID() + ";";
+            resultSet = statement.executeQuery(sql);
+            resultSet.beforeFirst();
+
+            while (resultSet.next()) {
+                admins.add(new Usuario(resultSet.getString("Nombre"), resultSet.getString("Apellido"),
+                        resultSet.getInt("Documento"), resultSet.getString("Email"),
+                        resultSet.getString("Password"), 1));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error en SQL" + ex);
+        } finally {
+            try {
+                System.out.println("cerrando statement...");
+                statement.close();
+                System.out.println("cerrando conexión...");
+                connection.close();
+            } catch (SQLException ex) {
+                System.out.println("Error en SQL" + ex);
+            }
+        }
+        return admins;
+
+    }
+
+    public ArrayList<Usuario> getAdminsOutLab(Laboratorio lab) {
+        ArrayList<Usuario> admins = new ArrayList<>();
+        ResultSet resultSet = null;
+
+        try {
+            connection = DBConnection.getConnection();
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM (SELECT Documento as DOC FROM "
+                    + "laboratorio_administrador_1, active_admins WHERE IDAdministrador = Documento AND IDLaboratorio ="
+                    + lab.getID() + ") IL RIGHT JOIN active_admins ADM"
+                    + " ON IL.DOC = ADM.Documento WHERE IL.DOC IS NULL;";
+            System.out.println(sql);
+            resultSet = statement.executeQuery(sql);
+            resultSet.beforeFirst();
+
+            while (resultSet.next()) {
+                admins.add(new Usuario(resultSet.getString("Nombre"), resultSet.getString("Apellido"),
+                        resultSet.getInt("Documento"), resultSet.getString("Email"),
+                        resultSet.getString("Password"), 1));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error en SQL" + ex);
+        } finally {
+            try {
+                System.out.println("cerrando statement...");
+                statement.close();
+                System.out.println("cerrando conexión...");
+                connection.close();
+            } catch (SQLException ex) {
+                System.out.println("Error en SQL" + ex);
+            }
+        }
+        return admins;
+
+    }
+
+    public boolean addAdmintoLab(Usuario usr, Laboratorio lab) {
+
+        int resultSet;
+
+        try {
+            resultSet = -1;
+            connection = DBConnection.getConnection();
+            statement = connection.createStatement();
+            String sql = "INSERT INTO laboratorio_administrador_1 (`IDLaboratorio`, `IDAdministrador`) Values("
+                    + lab.getID() + "," + usr.getDocumento() + ");";
+            resultSet = statement.executeUpdate(sql);
+            return resultSet > 0;
+        } catch (SQLException ex) {
+            System.out.println("Error en SQL" + ex);
+            return false;
+        } finally {
+            try {
+                System.out.println("cerrando statement...");
+                statement.close();
+                System.out.println("cerrando conexión...");
+
+                connection.close();
+
+            } catch (SQLException ex) {
+                System.out.println("Error en SQL" + ex);
+            }
+        }
+    }
+
+    public boolean removeAdminfromLab(Usuario usr, Laboratorio lab) {
+
+        int resultSet;
+
+        try {
+            resultSet = -1;
+            connection = DBConnection.getConnection();
+            statement = connection.createStatement();
+            String sql = "DELETE FROM laboratorio_administrador_1 WHERE IDLaboratorio =" + lab.getID() +
+                    " AND IDAdministrador =" + usr.getDocumento() + ";" ;
+            resultSet = statement.executeUpdate(sql);
+            return resultSet > 0;
+        } catch (SQLException ex) {
+            System.out.println("Error en SQL" + ex);
+            return false;
+        } finally {
+            try {
+                System.out.println("cerrando statement...");
+                statement.close();
+                System.out.println("cerrando conexión...");
+
+                connection.close();
+
+            } catch (SQLException ex) {
+                System.out.println("Error en SQL" + ex);
+            }
+        }
+    }
+
+    public boolean disableLab(Laboratorio lab) {
+        int resultSet;
 
         try {
             resultSet = -1;
