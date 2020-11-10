ccacc0f4227d38378c2eb5a8b8cd5efd381937db
niguzmang <niguzmang@unal.edu.co>
2020-11-09 23:09:27 -0500

    Interfaz gráfica creación categorías

--- src/Control/Admin/AdminInventario.java
+++ src/Control/Admin/AdminInventario.java
@@ -8,6 +8,7 @@ import Control.LoginController;
 import DAO.DBConnection;
 import Entidad.Laboratorio;
 import Entidad.Usuario;
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -18,10 +19,14 @@ import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 /**
  *
  * @author nguzman
@@ -51,8 +56,24 @@ public class AdminInventario implements Initializable{
         }
         table.setItems(oblist);
     }
+    private void ToPath(String path) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(path));
+            Parent root1 = (Parent) fxmlLoader.load();
+            
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setResizable(false);
+            stage.show();
+            Stage stage1 = (Stage) backbtn.getScene().getWindow();
+            stage1.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public void goToCategorias(){
-        System.out.println("ir a categorias");
+        ToPath("/Frontera/Admin/AdminCategorias.fxml");
     
     
     }
@@ -61,6 +82,8 @@ public class AdminInventario implements Initializable{
     
     
     }
+    @FXML
+    private JFXButton backbtn;
 
     @FXML
     private TableView<Laboratorio> table;
