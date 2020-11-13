828b64533b78738e87503d7e97133877bc3d61de
ancardonac <42379711+ancardonac@users.noreply.github.com>
2020-11-13 17:55:36 -0500

    Gestión de clase v2 - modificación laboratorioDAO

--- src/Control/Admin/AdminGestorClaseController.java
+++ src/Control/Admin/AdminGestorClaseController.java
@@ -5,9 +5,18 @@
  */
 package Control.Admin;
 
+import Control.LoginController;
+import DAO.LaboratorioDAO;
+import Entidad.Laboratorio;
+import Entidad.Usuario;
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -15,13 +24,51 @@ import javafx.fxml.Initializable;
  * @author ANDRES CAMILO
  */
 public class AdminGestorClaseController implements Initializable {
+    
+    private LaboratorioDAO dao = new LaboratorioDAO();
+    private Usuario user = LoginController.getUsuario();
+    private Laboratorio lab = AdminMenuController.currentLab;
+    
+    @FXML
+    private JFXButton gestionbtn;
+
+    @FXML
+    private RadioButton bloquearbtn;
+
+    @FXML
+    private RadioButton permitirbtn;
+
+    @FXML
+    void Gestión(ActionEvent event) {
+        if(this.bloquearbtn.isSelected()){
+            
+       dao.disableLab(lab);
+        
+        }else if(this.permitirbtn.isSelected()){
+       dao.enableLab(lab);
+        
+        }else{
+        
+        
+        
+        }
+
+    }
+    
+    
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        ToggleGroup tg = new ToggleGroup();
+        this.bloquearbtn.setToggleGroup(tg);
+        this.permitirbtn.setToggleGroup(tg);
     }    
+    
+    
+    
     
 }
