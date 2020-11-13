cdf84b98e43ea16287cc15777548af3b8472c56d
David Bricestone <29939205+jdbu2002@users.noreply.github.com>
2020-11-13 16:21:51 -0500

    Reserve

--- src/Control/User/UserReserverController.java
+++ src/Control/User/UserReserverController.java
@@ -1,23 +1,71 @@
 package Control.User;
 
 import Control.LoginController;
+import DAO.LaboratorioDAO;
+import Entidad.Laboratorio;
 import Entidad.Usuario;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
-public class UserReserverController {
+public class UserReserverController implements Initializable{
     private Usuario user = LoginController.getUsuario();
+    private ObservableList<Laboratorio> dataLabs;
     
+    
+    @FXML
+    private JFXComboBox<Laboratorio> labList;
+    @FXML
     private JFXButton backToMenu1;
     @FXML
     private JFXButton reserveButton;
     @FXML
     private JFXButton reserveButton1;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        getLabs();
+    }
+    
+    private Laboratorio findID(int id) {
+        for (Laboratorio lab : dataLabs) {
+            if (lab.getID() == id) {
+                return lab;
+            }
+
+        }
+        return null;
+    }
+    
+    private void getLabs() {
+        dataLabs = FXCollections.observableList(new LaboratorioDAO().getLabs());
+
+        labList.setItems(dataLabs);
+        labList.setConverter(new StringConverter<Laboratorio>() {
+            @Override
+            public String toString(Laboratorio object) {
+                return object.getID() + "-" + object.getNombre();
+            }
+
+            @Override
+            public Laboratorio fromString(String string) {
+                return findID(Integer.parseInt(string.split("-")[0]));
+            }
+        });
+
+    }
+
     
     private void ToPath(String path) {
         try {
