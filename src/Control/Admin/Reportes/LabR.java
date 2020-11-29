9b7a1b901a16212097d8baee9672ffb6311ed0d5
Sebastián <jupachonc@unal.edu.co>
2020-11-29 00:12:48 -0500

    InventarioReporte alpha

--- src/Control/Admin/Reportes/LabR.java
+++ src/Control/Admin/Reportes/LabR.java
@@ -7,9 +7,12 @@ package Control.Admin.Reportes;
 
 import Control.Admin.AdminMenuController;
 import Control.Admin.AdminReportes;
+import DAO.LaboratorioDAO;
 import DAO.ReportesDAO;
 import Entidad.Categoria;
+import Entidad.Elemento;
 import Entidad.Laboratorio;
+import Entidad.MacroCategoria;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
 import java.net.URL;
@@ -17,6 +20,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -60,6 +64,130 @@ public class LabR implements Initializable {
 
     @FXML
     private void onCurrentInventary(ActionEvent event) {
+        doReportI();
+    }
+
+    private boolean doReportI() {
+        try {
+            int filaidx = 0;
+
+            //Creación Libro y hoja
+            Workbook workbook = new XSSFWorkbook();
+            Sheet pagina = workbook.createSheet("Reporte");
+
+            //Titulo
+            pagina.addMergedRegion(new CellRangeAddress(filaidx, filaidx, 0, 3));
+            Row titleF = pagina.createRow(filaidx);
+            CellStyle titleStyle = workbook.createCellStyle();
+            Font titleFont = workbook.createFont();
+            titleFont.setBold(true);
+            titleFont.setFontHeightInPoints((short) 20);
+            titleStyle.setAlignment(HorizontalAlignment.CENTER);
+            titleStyle.setFont(titleFont);
+            Cell title = titleF.createCell(0);
+            title.setCellStyle(titleStyle);
+            title.setCellValue("Estado Actual Inventario Laboratorio " + lab.getID() + "-" + lab.getNombre());
+            filaidx++;
+
+            //CellStyles
+            //MacroCat
+            CellStyle mCatStyle = workbook.createCellStyle();
+            Font mCatFont = workbook.createFont();
+            mCatFont.setBold(true);
+            mCatFont.setFontHeightInPoints((short) 14);
+            mCatFont.setColor(IndexedColors.WHITE.getIndex());
+            mCatStyle.setFillForegroundColor(IndexedColors.BLUE.getIndex());
+            mCatStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            mCatStyle.setAlignment(HorizontalAlignment.CENTER);
+            mCatStyle.setFont(mCatFont);
+            //Cat
+            CellStyle CatStyle = workbook.createCellStyle();
+            Font CatFont = workbook.createFont();
+            CatFont.setFontHeightInPoints((short) 14);
+            CatFont.setColor(IndexedColors.WHITE.getIndex());
+            CatStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+            CatStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            CatStyle.setAlignment(HorizontalAlignment.LEFT);
+            CatStyle.setFont(CatFont);
+            //Header
+            CellStyle style = workbook.createCellStyle();
+            Font newFont = workbook.createFont();
+            newFont.setColor(IndexedColors.WHITE.getIndex());
+            style.setFillForegroundColor(IndexedColors.AQUA.getIndex());
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            style.setFont(newFont);
+
+            String[] titulos = {"ID", "Nombre Elemento", "Descripción",
+                "Estado"};
+
+            Row fila = pagina.createRow(filaidx);
+
+            filaidx++;
+
+            LaboratorioDAO labDAO = new LaboratorioDAO();
+
+            for (MacroCategoria mcat : labDAO.getMCats(lab.getID())) {
+                pagina.addMergedRegion(new CellRangeAddress(filaidx, filaidx, 0, 3));
+                fila = pagina.createRow(filaidx);
+                Cell mcell = fila.createCell(0);
+                mcell.setCellStyle(mCatStyle);
+                mcell.setCellValue(mcat.getID() + " - " + mcat.getNombre());
+                filaidx++;
+                for (Categoria cat : labDAO.getCats(mcat)) {
+                    pagina.addMergedRegion(new CellRangeAddress(filaidx, filaidx, 0, 3));
+                    fila = pagina.createRow(filaidx);
+                    Cell ccell = fila.createCell(0);
+                    ccell.setCellStyle(CatStyle);
+                    ccell.setCellValue(cat.getID() + " - " + cat.getNombre());
+                    filaidx++;
+                    ArrayList<Elemento> elements = labDAO.getElements(cat);
+                    if (!elements.isEmpty()) {
+                        fila = pagina.createRow(filaidx);
+                        for (int i = 0; i < titulos.length; i++) {
+                            // Creamos una celda en esa fila, en la posicion 
+                            // indicada por el contador del ciclo
+                            Cell celda = fila.createCell(i);
+
+                            // Indicamos el estilo que deseamos 
+                            // usar en la celda, en este caso el unico 
+                            // que hemos creado
+                            celda.setCellStyle(style);
+                            celda.setCellValue(titulos[i]);
+                        }
+                    }
+                    for (Elemento elm : labDAO.getElements(cat)) {
+                        int ci = 0;
+                        fila = pagina.createRow(filaidx);
+                        Cell cell = fila.createCell(ci);
+                        cell.setCellValue(elm.getID());
+                        ci++;
+                        cell = fila.createCell(ci);
+                        cell.setCellValue(elm.getNombre());
+                        ci++;
+                        cell = fila.createCell(ci);
+                        cell.setCellValue(elm.getDescripción());
+                        ci++;
+                        cell = fila.createCell(ci);
+                        cell.setCellValue(getState(elm.getEstado()));
+                        filaidx++;
+                    }
+
+                }
+
+            }
+            //Autosize Columns
+            for (int i = 0; i < titulos.length; i++) {
+                pagina.autoSizeColumn(i);
+            }
+
+            //Save file
+            AdminReportes.save(workbook, "Inventario_Lab_" + lab.getID() + "_" + LocalDate.now());
+
+            return true;
+        } catch (Exception ex) {
+            System.out.println("Excepción: " + ex);
+            return false;
+        }
     }
 
     private boolean doReportL(Laboratorio lab, LocalDateTime in, LocalDateTime fi) throws SQLException {
@@ -105,8 +233,8 @@ public class LabR implements Initializable {
             filaidx++;
 
             //Encabezados
-            String[] titulos = {"ID", "ID Elemento", "Nombre Elemento", "Categoría", 
-                "Macrocategoría","IDEstudiante", "Nombre Estudiante", "E-Mail",
+            String[] titulos = {"ID", "ID Elemento", "Nombre Elemento", "Categoría",
+                "Macrocategoría", "IDEstudiante", "Nombre Estudiante", "E-Mail",
                 "Administrador", "Tiempo Inicio", "Tiempo Entrega", "Tiempo Uso (Min)"};
 
             Font newFont = workbook.createFont();
@@ -220,13 +348,28 @@ public class LabR implements Initializable {
             alert.showAndWait();
             return;
         }
-        
+
         doReportL(lab, initialDate, endDate);
     }
 
     @FXML
     private void onSaveHistory(ActionEvent event) throws SQLException {
         doReportL(lab, null, null);
+    }
+
+    private String getState(int idx) {
+        switch (idx) {
+            case 0:
+                return "Disponible";
+            case 1:
+                return "En Préstamo";
+            case 2:
+                return "En Mantenimiento";
+            case 3:
+                return "Inhabilitado";
+
+        }
+        return "";
     }
 
 }
