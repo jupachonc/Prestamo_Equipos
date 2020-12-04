a5bc93380e9437061f0b0f00a9f1e0fba58fe5a9
Sebastián <jupachonc@unal.edu.co>
2020-12-04 14:31:24 -0500

    ComboBox onTable

--- src/Control/Admin/AdminPrestamoUser.java
+++ src/Control/Admin/AdminPrestamoUser.java
@@ -40,7 +40,8 @@ public class AdminPrestamoUser implements Initializable {
     private Usuario estudiante;
     private ObservableList<Elemento> elementos = FXCollections.observableArrayList();
     private ObservableList<String> options = FXCollections.observableArrayList();
-    
+    private int[] devstates;
+
     @FXML
     private JFXTreeTableView<Elemento> devoTable;
     @FXML
@@ -70,8 +71,8 @@ public class AdminPrestamoUser implements Initializable {
         if (isNumeric(DocumentN.getText())) {
             System.out.println(Integer.parseInt(DocumentN.getText()));
             estudiante = new UsuarioDAO().getUser(Integer.parseInt(DocumentN.getText()));
-            
-            if(estudiante == null){
+
+            if (estudiante == null) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Información");
                 alert.setHeaderText("El documento es inválido");
@@ -79,7 +80,7 @@ public class AdminPrestamoUser implements Initializable {
                 alert.showAndWait();
                 return;
             }
-            
+
             updateUser();
             getElementos();
         } else {
@@ -93,8 +94,8 @@ public class AdminPrestamoUser implements Initializable {
 
     private void getElementos() {
         Prestamo prst = new PrestamoDAO().getPrestamo(estudiante);
-        
-        if(prst == null){
+
+        if (prst == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Información");
             alert.setHeaderText("Este usuario no tiene préstamos activos");
@@ -102,54 +103,63 @@ public class AdminPrestamoUser implements Initializable {
             alert.showAndWait();
             return;
         }
-        
+
         elementos = FXCollections.observableList(new PrestamoDAO().getHistorySpecific(prst.getID()));
+        
+        devstates = new int[elementos.size()];
 
-            JFXTreeTableColumn<Elemento, String> settingsColumn = new JFXTreeTableColumn<>("Estado del Elemento");
-            settingsColumn.setPrefWidth(173);
-            Callback<TreeTableColumn<Elemento, String>, TreeTableCell<Elemento, String>> cellFactory
-                    = //
-                    (final TreeTableColumn<Elemento, String> param) -> {
-                        final TreeTableCell<Elemento, String> cell = new TreeTableCell<Elemento, String>() {
+        JFXTreeTableColumn<Elemento, String> settingsColumn = new JFXTreeTableColumn<>("Estado del Elemento");
+        settingsColumn.setPrefWidth(173);
+        Callback<TreeTableColumn<Elemento, String>, TreeTableCell<Elemento, String>> cellFactory
+                = //
+                (final TreeTableColumn<Elemento, String> param) -> {
+                    final TreeTableCell<Elemento, String> cell = new TreeTableCell<Elemento, String>() {
 
-                    final JFXComboBox opts = new JFXComboBox(options);
-                    
-                    @Override
-                    public void updateItem(String item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                            setText(null);
-                        } else {
-                            opts.setStyle("-jfx-label-float: true;");
-                            opts.getSelectionModel().getSelectedIndex();
-                            setGraphic(opts);
-                            setText(null);
-                        }
+                final JFXComboBox opts = new JFXComboBox(options);
+
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+                        opts.setStyle("-jfx-label-float: true;");
+                        opts.getSelectionModel().getSelectedIndex();
+                        setGraphic(opts);
+                        opts.setOnAction(e -> {
+                            int idx = this.getTreeTableRow().getIndex();
+                            JFXComboBox cb = (JFXComboBox) opts;
+                            devstates[idx] = cb.getSelectionModel().getSelectedIndex();
+                            System.out.println(devstates[idx]);
+                        });
+                        setText(null);
+
                     }
+                }
+            };
+                    return cell;
                 };
-                        return cell;
-                    };
 
-            settingsColumn.setCellFactory(cellFactory);
+        settingsColumn.setCellFactory(cellFactory);
 
-            devoTable.getColumns().set(3, settingsColumn);
+        devoTable.getColumns().set(3, settingsColumn);
 
-            idColumn.setCellValueFactory(
-                    new TreeItemPropertyValueFactory<>("ID")
-            );
-            
-            namePrestamo.setCellValueFactory(
-                    new TreeItemPropertyValueFactory<>("nombre")
-            );
-            descColumn.setCellValueFactory(
-                    new TreeItemPropertyValueFactory<>("descripción")
-            );
+        idColumn.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("ID")
+        );
 
-            TreeItem<Elemento> root = new RecursiveTreeItem<>(elementos, RecursiveTreeObject::getChildren);
+        namePrestamo.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("nombre")
+        );
+        descColumn.setCellValueFactory(
+                new TreeItemPropertyValueFactory<>("descripción")
+        );
 
-            devoTable.setRoot(root);
-            devoTable.setShowRoot(false);
+        TreeItem<Elemento> root = new RecursiveTreeItem<>(elementos, RecursiveTreeObject::getChildren);
+
+        devoTable.setRoot(root);
+        devoTable.setShowRoot(false);
 
     }
 
@@ -158,7 +168,7 @@ public class AdminPrestamoUser implements Initializable {
         LastName.setText(estudiante.getApellido());
         Document.setText(String.valueOf(estudiante.getDocumento()));
     }
-    
+
     @FXML
     private void BackToMenu(ActionEvent event) {
         try {
@@ -176,16 +186,16 @@ public class AdminPrestamoUser implements Initializable {
         }
     }
 
-    private void loadOptions(){
+    private void loadOptions() {
         options.add("Devolución Completa");
         options.add("Devolución Incompleta");
         options.add("Devolución con Daño");
     }
-    
+
     @FXML
-    private void makeDevolution(ActionEvent event){
-        
-        if(elementos.isEmpty()){
+    private void makeDevolution(ActionEvent event) {
+
+        if (elementos.isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Información");
             alert.setHeaderText("Este usuario no tiene préstamos activos");
@@ -193,26 +203,29 @@ public class AdminPrestamoUser implements Initializable {
             alert.showAndWait();
             return;
         }
-        
-        //Aquí debería colocar el for each, luego me encargo de revisar el índice.
-        
-        
-        if(prestamoReserva >= 0){
-            if(new PrestamoDAO().makeDevolution(reservas.get(0).getID(), prestamoReserva + 1, commens)){
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Información");
-                alert.setHeaderText("Devolución Completa");
-                alert.setContentText(null);
-                alert.showAndWait();
-            }
-            else{
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Información");
-                alert.setHeaderText("Ha ocurrido un error al actualizar el préstamo.");
-                alert.setContentText(null);
-                alert.showAndWait();
-            }
-        }
-        
+
+//        for (int i = 0; i < elementos.size(); i++) {
+//            System.out.println(devoTable.g);
+////            int idx = cmb.getSelectionModel().getSelectedIndex();
+////            System.out.println(idx);
+//
+//        }
+
+//        if(prestamoReserva >= 0){
+//            if(new PrestamoDAO().makeDevolution(reservas.get(0).getID(), prestamoReserva + 1, commens)){
+//                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//                alert.setTitle("Información");
+//                alert.setHeaderText("Devolución Completa");
+//                alert.setContentText(null);
+//                alert.showAndWait();
+//            }
+//            else{
+//                Alert alert = new Alert(Alert.AlertType.ERROR);
+//                alert.setTitle("Información");
+//                alert.setHeaderText("Ha ocurrido un error al actualizar el préstamo.");
+//                alert.setContentText(null);
+//                alert.showAndWait();
+//            }
+//        }
     }
 }
